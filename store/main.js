8d8084dfcb7389717e57c8d289209f3b0937921f
DynamoRed <raphael@dynamored.com>
2021-11-22 16:52:57 +0000

    Init base template

---
+++ store/main.js
@@ -0,0 +1,19 @@
+export let state = () => ({
+    maintenance: true
+})
+
+export const getters = {
+
+}
+
+export const mutations = {
+
+}
+
+export const actions = {
+
+}
+
+export const modules = {
+
+}
